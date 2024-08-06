2d7f57d0a16be875c3cd672d732c9a93760636a7
gtech29 <juan.rodriguez.517@my.csun.edu>
2024-08-05 17:20:05 -0700

    updating website

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
-$(document).ready(function ()
-{
-    $('a[href^="#"]').on('click', function (e)
-    {
-        e.preventDefault();
+$(document).ready(function () {
+  $('a[href^="#"]').on("click", function (e) {
+    e.preventDefault();
 
-        var target = this.hash;
-        var $target = $(target);
+    var target = this.hash;
+    var $target = $(target);
 
-        $('html, body').stop().animate(
-            {
-                'scrollTop': $target.offset().top
-            }, 1500, 'swing', function ()
-            {
-                window.location.hash = target;
-            });
-    });
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $target.offset().top,
+        },
+        1500,
+        "swing",
+        function () {
+          window.location.hash = target;
+        }
+      );
+  });
 });
